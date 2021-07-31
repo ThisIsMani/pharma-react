62448d5df18db96b214bd52072fc3728abe35f67
manic <manichandratinku1234@gmail.com>
2021-07-31 20:48:29 +0530

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -30,9 +30,6 @@ const generator = new idGenerator.Generator();
 
 const PORT = process.env.PORT || 3001;
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
-
 app.use(cors())
 
 app.get('/product/:id', async (req, res) => {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
-window.$server = ''
+window.$server = 'http://localhost:3001'
 
 ReactDOM.render(
   <BrowserRouter>
