ca60ce4125edb9efcaa3875aef065c8954a24119
manic <manichandratinku1234@gmail.com>
2021-07-31 20:20:36 +0530

    app commit 1

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const path = require('path');
+const csrf = require('csurf');
+const bodyParser = require("body-parser");
+const idGenerator = require("firebase-auto-ids")
+const os = require('os')
+const fs = require('fs')
+const Busboy = require('busboy');
+let UUID = require('uuid-v4');
+const cors = require('cors')
+
+const serviceAccountKey = require('./serviceAccountKey.json');
+const cookieParser = require('cookie-parser');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountKey),
+    databaseURL: "https://pharma-9e4a8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    storageBucket: 'pharma-9e4a8.appspot.com'
+});
+
+const db = admin.firestore();
+const bucket = admin.storage().bucket();
+
+const csrfMiddleware = csrf({cookie: true});
+
+const port = process.env.PORT || 3000;
+const app = express();
+const generator = new idGenerator.Generator();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors())
+
+app.get('/product/:id', async (req, res) => {
+    const id = req.params.id;
+    const cardResult = await (await db.collection('cards').doc(id).get()).data();
+    res.json({productDetails: cardResult})
+})
+
+app.get('/search', async (req, res) => {
+    const q = req.query.q;
+    const searchResultCards = [];
+    const casSearch = await db.collection('cards').where('casLower', '==', q.toLowerCase()).get();
+    const titleSearch = await db.collection('cards').where('titleLower', '==', q.toLowerCase()).get();
+    casSearch.forEach((doc) => {
+        searchResultCards.push(doc.data());
+    });
+    titleSearch.forEach((doc) => {
+        searchResultCards.push(doc.data());
+    });
+    console.log(searchResultCards.length)
+    res.json({searchResults: searchResultCards});
+});
+
+app.get('/topProducts', async (req, res) => {
+    const searchResultCards = [];
+    const casSearch = await db.collection('cards').get();
+    casSearch.forEach((doc) => {
+        searchResultCards.push(doc.data());
+    });
+    res.json({topProducts: searchResultCards})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import React from "react";
+import {Redirect, Route} from 'react-router-dom';
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import HomePage from './pages/HomePage';
+import SearchPage from "./pages/SearchPage";
+import ProductDetails from "./pages/ProductDetails";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+
+            <Route exact path='/'>
+                <Redirect to='/home'/>
+            </Route>
+            <Route path='/home'>
+                <HomePage/>
+            </Route>
+            <Route path='/search'>
+                <SearchPage/>
+            </Route>
+            <Route path='/products/:id'>
+                <ProductDetails/>
+            </Route>
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
