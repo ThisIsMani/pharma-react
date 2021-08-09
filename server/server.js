1238a0afb39212e36e4407015860c770fd2b7b8e
manic <manichandratinku1234@gmail.com>
2021-08-09 13:17:03 +0530

    for heroku

--- server/server.js
+++ server/server.js
@@ -72,9 +72,9 @@ app.get('/topProducts', async (req, res) => {
     res.json({topProducts: searchResultCards})
 })
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
